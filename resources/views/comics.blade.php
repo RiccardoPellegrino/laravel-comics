123c12ec62ce8ef19e2f65431180a39616a40953
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-12-15 19:36:24 +0100

    migliorate bozze card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="pt-5">
 <div class="mio_container">
 @foreach ($comics as $comic)
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 14rem;">
     <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">{{$comic['title']}}</h5>
+      <h5 class="card-title text-white">{{$comic['title']}}</h5>
     </div>
   </div>
   @endforeach
+</div>
 </div>
 @endsection
