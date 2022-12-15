0bd94318655ac5e1f098e2123020fc2bf911a5cb
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-12-15 21:00:18 +0100

    bozza footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,18 @@
-<footer class="d-flex justify-content-center">
-<span class="text-white fs-1">Made with Love XD</span>
+<footer>
+    <div class="bottom-footer">
+        <div class="container d-flex">
+            <div>
+            <button>sign-up now!</button>
+            </div>
+            <div class="ft">
+                <span>follow us</span>
+                <a><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook"></a>
+                <a><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter"></a>
+                <a><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube"></a>
+                <a><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest"></a>
+                <a><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope"></a>
+            </div>
+        </div>
+   </div>
 </footer>
 
